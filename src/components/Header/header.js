19f594d6bd4616ea6d2669c65e23f3781cdc228d
sideeq12 <abdwaheed2018@gmail.com>
2021-02-02 13:47:41 +0100

    cleared

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return ( <div className="Header row">
+        <div className="col-md-4">
+            <img src=""/>
+        </div>
+        <div className="col-md-6 side-text">
+        Text placement
+        </div>
+    </div>
+     );
+}
+ 
+export default Header;
