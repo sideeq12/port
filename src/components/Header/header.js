6cd36a68da567405438cf6b1fcf5734a273852ba
sideeq12 <abdwaheed2018@gmail.com>
2021-02-02 14:27:59 +0100

    header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,10 +1,24 @@
+import "./header.css"
+import image from "./sideeq.jpg"
 const Header = () => {
-    return ( <div className="Header row">
+    return ( <div className="header row">
         <div className="col-md-4">
-            <img src=""/>
+            <img src={image} className="image"/>
         </div>
-        <div className="col-md-6 side-text">
-        Text placement
+        <div className="col-md-8 side-text">
+        <div className="dash">
+            <div className="dashname">
+                <h6>Waheed Sodiq A.</h6>
+                <label>Frontend Developer</label>
+            </div>
+            <div className="dash-adress">
+                Abdwaheed2018@gmail.com<br/>
+                (+234)-811-390-6263
+            </div>
+        </div>
+        <p>Self motivated Developer who is willing to learn and create outstanding UI application.
+        Self motivated Developer who is willing to learn and create outstanding UI application
+        .</p>
         </div>
     </div>
      );
