529229c92f88ed2255a1ab2592c58a18057df59a
sideeq12 <abdwaheed2018@gmail.com>
2021-02-03 12:32:18 +0100

    image set

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,15 @@
 import "./projects.css"
 import Project from "./project"
+import Recipe from "./recipe.jpg"
+import Gallery from "./gallery.jpg"
+import Checkout from "./checkout.jpg"
 
 const Projects = () => {
-   
+   const RecipeArticle="In this project, I design a Recipe page with HTML and CSS, the design is gotten from devChallenge.io and pretty inspiring."
     return (<div  className="work-wrapper">
-        <Project />
-        <Project />
-        <Project />
+        <Project image={Recipe} Head="Recipe blog"  paragraph={RecipeArticle}/>
+        <Project image={Gallery}  Head="Gallery Page" paragraph={RecipeArticle}/>
+        <Project image={Gallery}  Head="Checkout page" paragraph={RecipeArticle}/>
     </div> );
 }
  
--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,23 +1,27 @@
 import "./project.css"
-import image from "./image.png"
+
 
 const style = {
     display : "flex",
     flexDirection : "row",
     justifyContent : "space-between"
 }
-const Project = () => {
+const imageStyle ={
+    height : "60%",
+    width: "100%"
+}
+const Project = ({image, Head, paragraph, code, preview}) => {
     return ( <div className="work">
-    <div className="work-image">
-        <img src={image} alt="" style={{width : "100%"}}/>
+    <div className="work-image" style={imageStyle}>
+        <img src={image} alt="" style={{width : "100%", height : "100%"}}/>
     </div>
     <div className="stories">
-    <h5 style={{marginTop : "5px"}} >Recipe Blog</h5>
-    <p>TIn this project, I work with HTML and Css to create a responsive Recipe blog page. The design is from DevChallenge.io</p>
+    <h5 style={{marginTop : "5px"}} >{Head}</h5>
+    <p>{paragraph}</p>
     </div>
     <div className="preview" style={style}>
-    <div className="btn btn-primary">Demo</div>
-    <div className="btn">Code</div>
+    <div className="btn btn-primary">{preview}</div>
+    <div className="btn">{code}</div>
     </div>
     </div>);
 }
